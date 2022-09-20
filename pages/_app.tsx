dd2d461d6d438633be481aa56e0b5abdd0fa9c6c
Harsh Pareek <harshpareek@Harshs-MacBook-Air-2.local>
2022-09-21 03:39:50 +0530

    unnecessary file deletion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import "@fontsource/poppins";
-import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
-import "../styles/globals.css";
 import theme from "../themes/lightTheme";
 import createEmotionCache from "../cache/createEmotionCache";
 import NextNProgress from "nextjs-progressbar";
