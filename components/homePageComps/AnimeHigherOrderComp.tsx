c11a2f766a12c40908d21f2f8236aef3fde28149
Harsh Pareek <harshpareek@Harshs-MacBook-Air-2.local>
2022-09-21 07:30:47 +0530

    dependency error resolved

--- components/homePageComps/AnimeHigherOrderComp.tsx
+++ components/homePageComps/AnimeHigherOrderComp.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/system";
 import React from "react";
 import AnimeCard from "./AnimeCard";
-import AnimeOne from "../../assets/png/saly-1.png";
-import AnimeTwo from "../../assets/png/saly-2.png";
-import AnimeThree from "../../assets/png/saly-12.png";
-import AnimeFour from "../../assets/png/saly-19.png";
-import AnimeFive from "../../assets/png/saly-22.png";
+import AnimeOne from "../../assets/png/Saly-1.png";
+import AnimeTwo from "../../assets/png/Saly-2.png";
+import AnimeThree from "../../assets/png/Saly-12.png";
+import AnimeFour from "../../assets/png/Saly-19.png";
+import AnimeFive from "../../assets/png/Saly-22.png";
 
 const AnimeHigherOrderComp = () => {
   return (
